9c8ece1d0ec0c98834cce7c8c7ab0410c572c21b
cmondragon <cmondragon.147@gmail.com>
2023-09-21 09:29:14 -0400

    bump golangci-lint

--- config/provider.go
+++ config/provider.go
@@ -33,8 +33,6 @@ var providerMetadata string
 // GetProvider returns provider configuration
 func GetProvider() *ujconfig.Provider {
 	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
-		//ujconfig.WithShortName("vultr"),
-		//ujconfig.WithRootGroup("vultr.upbound.io"),
 		ujconfig.WithIncludeList(ExternalNameConfigured()),
 		ujconfig.WithFeaturesPackage("internal/features"),
 		ujconfig.WithDefaultResourceOptions(
